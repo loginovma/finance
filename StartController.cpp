5b7e81eaa65a6bb9b9a65862b4dc423c0f5036f1
Mikhail Loginov <my.reg24012014@gmail.com>
2017-05-04 07:48:47 +0000

    Add menu cycle to main

--- StartController.cpp
+++ StartController.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "StartController.h"
 
+using namespace std;
+
 void StartController::run()
 {
-    Bank sberbank("sberbank");
+    cout << "Welcome to personal finance" << endl;
+    cout << "Please choose your option from the menu:" << endl;
+    /*Bank sberbank("sberbank");
     
     Account girokonto("1245", sberbank);
     
     std::cout << "Your acc number is " << girokonto.getNumber() << std::endl;
-    std::cout << "Your bank is " << girokonto.getBankName() << std::endl;
+    std::cout << "Your bank is " << girokonto.getBankName() << std::endl;*/
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
+#include <iostream>
+
 #include "account.h"
 #include "bank.h"
 #include "StartController.h"
+#include "BanksController.h"
 
 int main(int argc, char ** argv)
 {
     StartController app;
     
     app.run();
+    
+    int option = 0;
+    
+    while (1) {
+        std::cin >> option;
+        
+        if (!option) {
+            break;
+        }
+        
+        std::cout << option << std::endl;
+        
+        
+    }
+    
     return 0;
 }
