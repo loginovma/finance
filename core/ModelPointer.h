119d0dc9ce1fb729045ea93cfc73604a88f50891
Mikhail Loginov <my.reg24012014@gmail.com>
2017-05-06 21:40:30 +0000

    Add ModelPointer - shared pointer to Model

---
+++ core/ModelPointer.h
@@ -0,0 +1,38 @@
+#ifndef MODELPOINTER_H_
+#define MODELPOINTER_H_
+
+
+class ModelStorage
+{
+private:
+    Model* pObj;
+    int counter;
+    
+public:
+    void storeObject(Model* pObj);
+    void incrCount();
+    void decrCount();
+    int getStorageCount();
+    Model* getpObject();
+    
+    ModelStorage(Model* pObj);
+    ~ModelStorage();
+    
+};
+
+class ModelPointer
+{
+private:
+    ModelStorage* container;
+    
+public:
+    ModelPointer(Model* pObj);
+    ~ModelPointer();
+    Model* ptr() const;
+    
+    //Copy Constructor and operator= overload
+    ModelPointer(const ModelPointer &p);
+    ModelPointer& operator= (const ModelPointer p);
+};
+
+#endif
